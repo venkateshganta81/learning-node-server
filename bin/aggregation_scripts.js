4491754234be761099ff18a873b4ebf2f2e6c05c
srinikandula <srini.kandula@shodogg.com>
2018-06-12 16:32:20 +0530

    Change the match condition in aggregation

--- bin/aggregation_scripts.js
+++ bin/aggregation_scripts.js
@@ -22,10 +22,10 @@ db.inventories.createIndex({"BookedDate":1,"PGType":1})
  * Script to create aggregation of payment gateway collections by date
  */
 db.pgDateAggregation.remove({})
-db.inventories.aggregate([{$group: {_id: { PGType: "$PGType", BookedDate: "$BookedDate" },
-        count: { $sum: 1 },TicketAmount: { $sum: "$TicketAmount" }}}])
-        .forEach(function(data) {db.pgDateAggregation.insert(data)})
-
+db.inventories.aggregate([{ $match: {TicketAmount:{$ne :0} } },
+    {$group: {_id: { PGType: "$PGType", BookedDate: "$BookedDate" },
+            count: { $sum: 1 },TicketAmount: { $sum: "$TicketAmount" }}}])
+    .forEach(function(data) {db.pgDateAggregation.insert(data)})
 
 /**
  *  Script for reshuffling the data for Operatornames
