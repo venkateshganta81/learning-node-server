1b3442243e762419f11efe1ba4bb89b58a78ef42
srinikandula <srini.kandula@shodogg.com>
2018-06-13 12:35:11 +0530

    API added for aggregated Operator transactions and individual transactions by date

--- server/modules/operatorWiseInventory.js
+++ server/modules/operatorWiseInventory.js
@@ -9,7 +9,7 @@ var InventoryColl = require("../models/schemas").InventoryColl;
 var operatorAggregation = require("../models/schemas").operatorAggregation;
 
 var config = require("../config/config");
-
+/*
 operatorWiseInventory.prototype.getOperatorWiseInventory = function (callback) {
     var retObj = {};
     async.parallel({
@@ -29,6 +29,55 @@ operatorWiseInventory.prototype.getOperatorWiseInventory = function (callback) {
         retObj.status = true;
         callback(retObj);
     });
+}
+
+*/
+
+
+operatorWiseInventory.prototype.getOperatorWiseInventory = function (callback) {
+    var retObj = {};
+    operatorAggregation.aggregate([{$group: {_id: { OperatorName: "$_id.OperatorName"},
+                bookings:{$sum:"$bookings" },seats:{$sum:"$seats"},
+                TicketAmount: { $sum: "$TicketAmount"}}}],
+        function (err, operatorWiseInventory) {
+            if (err) {
+                retObj.status = false;
+                retObj.message = "Error While Getting Inventory";
+                callback(retObj);
+            } else if (operatorWiseInventory) {
+                console.log('operatorWiseInventory',operatorWiseInventory)
+                retObj.status = true;
+                retObj.message = "Inventory Found";
+                retObj.data = operatorWiseInventory;
+                callback(retObj);
+            } else {
+                retObj.status = false;
+                retObj.message = "No Data was found";
+                callback(retObj);
+            }
+        })
+}
+
+
+operatorWiseInventory.prototype.getOperatorBookings = function (operatorName, callback) {
+    var retObj = {};
+    operatorAggregation.find({"_id.OperatorName": operatorName}).exec(
+        function (err, operatorTransactions) {
+            if (err) {
+                retObj.status = false;
+                retObj.message = "Error While Getting Inventory";
+                callback(retObj);
+            } else if (operatorTransactions) {
+                retObj.status = true;
+                retObj.message = "Transactions Found";
+                retObj.data = operatorTransactions;
+                callback(retObj);
+            } else {
+                retObj.status = false;
+                retObj.message = "No Data was found";
+                callback(retObj);
+            }
+        })
 }
 
 module.exports = new operatorWiseInventory();
